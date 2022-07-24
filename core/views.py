8a10922eee61e6d6746c1f1901db072cade0e890
Diego Carlos <diego_carlos110@hotmail.com>
2022-07-24 19:54:37 -0300

    uls.py e views.py modificadas, juntamento com teplates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+#criação das views
+
+#view index
+class IndexView(TemplateView):
+    template_name = 'index.html'
