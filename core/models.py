0fc9bb2219d1ffe048357524cb33841db7201889
Diego Carlos <diego_carlos110@hotmail.com>
2022-07-26 21:02:00 -0300

    criado aplicação usuario

--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from stdimage import StdImageField
+import uuid
 
 # Create your models here.
+def get_file_path(_instance, filename):
+	'''função para gerar um id unico, que será passado na hora de gerar no caminho e no da imagem'''
+	ext = filename.split('.')[-1]
+	filename = f'{uuid.uuid4()}.{ext}'
+	return filename
+
+class Base(models.Model):
+    criado = models.DateTimeField('Criado em', auto_now_add=True)
+    modificado = models.DateTimeField('Modificado em', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+class Post(Base):
+    titulo_post = models.CharField(max_length=255, verbose_name='Título')
+    autor_post = models.CharField(max_length=255, verbose_name='Autor')
+    conteudo_post = models.TextField(verbose_name='Conteúdo')
+    imagem = StdImageField('Imagem', upload_to=get_file_path,
+                           variations={'thumb': {"width": 480, "height": 480, "crop": True}})
+
--- site_blog/settings.py
+++ site_blog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'usuarios',
     'core',
 ]
 
@@ -123,7 +123,12 @@ MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+AUTH_USER_MODEL = 'usuarios.CustomUsuario'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
