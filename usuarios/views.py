525caf38302a2498d96b06ba7d96b64d18a7249e
Diego Carlos <diego_carlos110@hotmail.com>
2022-07-26 22:34:38 -0300

    modificado

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import CustomUsuarioCreateForm, CustomUsuarioChangeForm
+
+def cadastro(request):
+    if request.method == 'POST':
+        form = CustomUsuarioCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'cadastro.html', {'form': form})
+    else:
+        form = CustomUsuarioCreateForm()
+    return render(request, 'cadastro.html', {'form': form})
